fcbc7f634fe9d3d551661b04e9575bf78cb94441
mohamadi <mohamadi.hamid@gmail.com>
2016-05-30 16:21:47 -0700

    ntcard.cpp: style fixed

--- ntcard.cpp
+++ ntcard.cpp
@@ -15,34 +15,34 @@ void printBin(uint64_t n) {
         }
         else
             printf("0");
-        
+
         n <<= 1;
     }
     printf("\n");
 }
 
 void test() {
-	uint64_t hVal= 0x00F000000000FF0;
-	printBin(hVal);
-	cout << __builtin_clzll(hVal) << "\n";
-	cout << __builtin_ctzll(hVal) << "\n";
-	exit(0);
+    uint64_t hVal= 0x00F000000000FF0;
+    printBin(hVal);
+    cout << __builtin_clzll(hVal) << "\n";
+    cout << __builtin_ctzll(hVal) << "\n";
+    exit(0);
 }
 
-int main(int argc, const char *argv[]){
-	test();
-	hip_estimator::hip_estimator<std::string> h;
-	std::ifstream in(argv[1]);
-	int k=atoi(argv[2]);
-	string hseq,seq;
-        while(getline(in,hseq)) {
-                getline(in,seq);
-                getline(in,hseq);
-                getline(in,hseq);
-		for(unsigned i=0; i<seq.length()-k+1;i++)
-			h.insert(seq.substr(i,k));
-        }
+int main(int argc, const char *argv[]) {
+    test();
+    hip_estimator::hip_estimator<std::string> h;
+    std::ifstream in(argv[1]);
+    int k=atoi(argv[2]);
+    string hseq,seq;
+    while(getline(in,hseq)) {
+        getline(in,seq);
+        getline(in,hseq);
+        getline(in,hseq);
+        for(unsigned i=0; i<seq.length()-k+1; i++)
+            h.insert(seq.substr(i,k));
+    }
     std::cout << h.count() << std::endl;
-	return 0;
+    return 0;
 }
 
